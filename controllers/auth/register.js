2aa184e199ea30d146881af7717227f230368007
Jakub Hypki <kubahypki7@gmail.com>
2024-05-01 22:31:03 +0200

    added description to bugs get response

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,47 +1,53 @@
-const db = require('../../db')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const axios = require('axios')
+const db = require("../../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
 
 async function register(req, res) {
-    const { firstName, secondName, email, password, token } = req.body
-    
-    try {
-        const response = await axios.post(
-            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.reCAPTCHA_SECRET_KEY}&response=${token}`
-        );
+  const { firstName, secondName, email, password, token } = req.body;
 
-        if (!response.data.success) {
-            return res.status(500).json({
-                error: 'captcha ignored'
-            })
-        } 
-    } catch (error) {
-        return res.status(500).send("Error verifying reCAPTCHA")
+  try {
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.reCAPTCHA_SECRET_KEY}&response=${token}`
+    );
+
+    if (!response.data.success) {
+      return res.status(500).json({
+        error: "captcha ignored",
+      });
     }
-    
-    const expectedUser = await db.query('SELECT * FROM users WHERE email = $1::text', [email])
+  } catch (error) {
+    return res.status(500).send("Error verifying reCAPTCHA");
+  }
 
-    if (expectedUser.rowCount > 0) {
-        return res.json({
-            error: 'already exist'
-        })
-    }
+  const expectedUser = await db.query(
+    "SELECT * FROM users WHERE email = $1::text",
+    [email]
+  );
 
-    const encryptedPassword = await bcrypt.hash(password, 10)
+  if (expectedUser.rowCount > 0) {
+    return res.json({
+      error: "already exist",
+    });
+  }
 
-    await db.query(`INSERT INTO users (first_name, second_name, email, password_hash) VALUES($1, $2, $3, $4)`,
-                                  [firstName, secondName, email, encryptedPassword])
+  const encryptedPassword = await bcrypt.hash(password, 10);
 
-    const accessToken = jwt.sign({ email }, process.env.JWT_ACCESS_SECRET, { 'expiresIn': '1d' });
-    
-    return res.status(200).json({
-        accessToken,
-        firstName,
-        secondName,
-        email
-    })
+  await db.query(
+    `INSERT INTO users (first_name, second_name, email, password_hash) VALUES($1, $2, $3, $4)`,
+    [firstName, secondName, email, encryptedPassword]
+  );
 
+  const accessToken = jwt.sign({ email }, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return res.status(200).json({
+    accessToken,
+    firstName,
+    secondName,
+    email,
+  });
 }
 
-module.exports = register
+module.exports = register;
